56618a2b42eef91e6912fed893746653b2507dfc
ong-osmond <ong.osmond@yahoo.com>
2020-10-06 16:35:47 +0800

    Revert "Added user registration and login features"
    This reverts commit 5c92e702640513820272db046e82706c58ff69a8.

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: process.env.MONGODB_URI || "mongodb://localhost/cfc-wa",
-    secretOrKey: "secret" 
-  };
--- server.js
+++ server.js
@@ -1,36 +1,19 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-//const routes = require("./routes");
-const passport = require("passport");
-const users = require("./routes/api/users");
 
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
-// Bodyparser middleware
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
-app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Passport middleware
-app.use(passport.initialize());
-// Passport config
-require("./config/passport")(passport);
-
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-//app.use(routes);
-app.use("/api/users", users);
+app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBooks"),
@@ -41,11 +24,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBooks"),
   useFindAndModify: false
 }
 ;
-
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 
 // Start the API server
 app.listen(PORT, function() {
